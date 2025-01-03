bc2cd6ce172675cdd8e9ce2d058956853bab4d37
Katlego R. Phele <113664624+katlegorphele@users.noreply.github.com>
2025-01-03 19:08:44 +0000

    added main components and utils

---
+++ next_app/utils/constants.js
@@ -0,0 +1,7 @@
+import abi from "./Transactions.json";
+import uzarAbi from "./abizar.json"
+
+export const contractAddress = "0x423ecc150E525a62Af7Ff00a2B03f1A298E2d11B";
+export const contractABI = abi;
+export const contractAddressUzar = "0xBf715EB900bbEAa2C7e136E9c2A0C6AED93E8aeb"
+export const contractUzarAbi = uzarAbi;
